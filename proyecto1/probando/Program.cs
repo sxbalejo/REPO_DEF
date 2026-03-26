09b1582c33d989611e74b27619870c5e6b9497ee
sxbalejo <sebastianleandro0108@gmail.com>
2026-03-26 19:02:53 -0400

    feat: limpieza de configuración y código base

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Sebastian!");
-
-string stripe_api_key = "sk_test_4eC39HqLyjWDarjttT1zdp7dc";
-Console.WriteLine($"Stripe API Key: {stripe_api_key}");
